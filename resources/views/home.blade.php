ad0263245060b1becb7b96de27b6048babd2cc11
Nuruzzaman Milon <contact@milon.im>
2019-11-07 08:25:15 +0100

    added user agent

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -15,7 +15,7 @@
                                 <td>Url</td>
                                 <td>Shorten Link</td>
                                 <td>Visits</td>
-                                <td></td>
+                                <td>Created At</td>
                             </tr>
                         </thead>
 
@@ -25,7 +25,7 @@
                                     <td>{{ $link->url }}</td>
                                     <td>{{ url($link->hash) }}</td>
                                     <td>{{ $link->counter }}</td>
-                                    <td></td>
+                                    <td>{{ $link->created_at->diffForHumans() }}</td>
                                 </tr>
                             @endforeach
                         </tbody>
